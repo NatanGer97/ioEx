fc16dcc5b1abc64eca435ea49096b9e386017456
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2021-11-13 17:52:12 +0200

    final version of first assignment

--- src/GetAllListFromCurrenentPath.java
+++ src/GetAllListFromCurrenentPath.java
@@ -7,15 +7,31 @@ public class GetAllListFromCurrenentPath
      * of all files and all folders in the current directory.
      */
     public static void main(String[] args) {
+        //  i gave an empty string  in file ct'or cause it create an empty path
         String currentDirection = new File("").getAbsolutePath();
+
         System.out.println(currentDirection);
+
         File[] files = new File(currentDirection).listFiles();
-        for(File file:files)
-        {
-            System.out.println(file);
-
-
+//        File[] files = new File("").listFiles();
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file);
+            }
         }
+
+//        try {
+//            for(File file:files) {
+//                System.out.println(file);
+//
+//            }
+//        }catch (NullPointerException e)
+//        {
+//            System.out.println(e.getMessage());
+//
+//        }
+
+
 
 
     }
